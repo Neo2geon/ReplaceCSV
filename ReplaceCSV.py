ba22505ccfa738ad3e6ea2fcd69fa90e7c6b11f1
Stevan Lopes <neo2geon@gmail.com>
2021-10-26 20:19:53 -0300

    Versão 1.0.1 - adicionado um close para o arquivo inicial.

--- ReplaceCSV.py
+++ ReplaceCSV.py
@@ -2,6 +2,7 @@ def replaceCSV(nome_do_arquivo: str, char_q_vai_sair: str, char_q_vai_entrar: st
     'Substitui caracteres em um CSV'
     arquivo = open(nome_do_arquivo, 'r')
     textoArquivo = arquivo.read()
+    arquivo.close()
     textoTransformado = textoArquivo.replace(char_q_vai_sair, char_q_vai_entrar)
     arquivoTemporario = open(nome_do_arquivo, 'w', encoding='utf-8')
     arquivoTemporario.write(textoTransformado)
